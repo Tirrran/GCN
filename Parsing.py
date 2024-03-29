1ecd3b61e8b11f8bff10085f92560db1e21ccdc6
2igor0 <154368462+2igor0@users.noreply.github.com>
2024-03-29 21:09:04 +0400

    Create Parsing.py

---
+++ Parsing.py
@@ -0,0 +1,29 @@
+# Парсинг транзакций в реальном времени (ХЭШ)
+
+import requests
+import time
+
+def get_unconfirmed_transactions():
+    url = "https://blockchain.info/unconfirmed-transactions?format=json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        transactions = data['txs']
+        return transactions
+    else:
+        print("Error:", response.status_code)
+        return None
+
+def main():
+    while True:
+        unconfirmed_transactions = get_unconfirmed_transactions()
+        if unconfirmed_transactions:
+            print("Транзакции:")
+            for tx in unconfirmed_transactions:
+                print("Хэш транзакции:", tx['hash'])
+        else:
+            print("Транзакций не обнаружено")
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
